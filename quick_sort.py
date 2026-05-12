9bd02b87db53d16f7c17f60879855246705d23b1
AnanyaPrashanth008 <ananya.prashanth7feb2008@gmail.com>
2026-05-12 17:21:52 +0530

    added QuickSort program

---
+++ quick_sort.py
@@ -0,0 +1,7 @@
+import partition at pt
+
+def quick_sort(numbers,low,high):
+    if low < high:
+        pivot_index = pt.partition_array(numbers, low, high)
+        quick_sort(numbers,low,pivot_index - 1)
+        quick_sort(numbers,pivot_index + 1,high)
